4ba825197d46d06fb79c8471ae176d01a22a44ec
Adrian Smith <adrian@17od.com>
2022-04-10 21:23:10 +0100

    Added metrics

--- internal/wolverine.go
+++ internal/wolverine.go
@@ -8,23 +8,40 @@ import (
 )
 
 var (
-	urlUp = prometheus.NewGauge(prometheus.GaugeOpts{
+	urlUpMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "sample_external_url_up",
 		Help: "Is the URL up (1) or down (0).",
-	})
+	},
+		[]string{"url"})
+
+	responseTimeMetric = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "sample_external_url_response_ms",
+		Help:    "Response time in milliseconds.",
+		Buckets: []float64{50, 100, 250, 500, 750, 1000},
+	},
+		[]string{"url"},
+	)
 )
 
 func init() {
-	prometheus.MustRegister(urlUp)
+	prometheus.MustRegister(urlUpMetric)
+	prometheus.MustRegister(responseTimeMetric)
 }
 
 func MonitorURL(url string, httpClient *http.Client) (error, *http.Response) {
 	start := time.Now()
 	resp, err := httpClient.Head(url)
 	if err != nil {
-		log.Printf("Error: %s", err)
+		log.Printf("Error: %v", err)
+		urlUpMetric.With(prometheus.Labels{"url": url}).Set(1)
 	} else {
 		duration := time.Since(start)
+		up := 0
+		if resp.StatusCode == 200 {
+			up = 1
+		}
+		urlUpMetric.With(prometheus.Labels{"url": url}).Set(float64(up))
+		responseTimeMetric.With(prometheus.Labels{"url": url}).Observe(float64(duration.Milliseconds()))
 		log.Printf("URL: %s, Status: %d, Duration: %dms", url, resp.StatusCode, duration.Milliseconds())
 	}
 	return err, resp
